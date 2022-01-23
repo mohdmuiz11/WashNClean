b0e3525e8f02354ae489229ffb5244a0bb95c20e
mohdmuiz11 <muiz_00@protonmail.com>
2022-01-23 22:58:11 +0800

    Small changes, not to disturb with order track

--- screens/homepage.js
+++ screens/homepage.js
@@ -101,7 +101,7 @@ function homepage ({navigation}){
     <Image style={stylesWNC.Location} source={require('../assets/location.png')}/>
   </TouchableOpacity>
 
-  <TouchableOpacity style={stylesWNC.CTracker} onPress={() => navigation.navigate("OrderTrack")}>
+  <TouchableOpacity style={stylesWNC.CTracker} onPress={() => navigation.navigate("OrderForm")}>
     <Text style={stylesWNC.navtext}>Tracker</Text>
     <Image style={stylesWNC.Tracker} source={require('../assets/track.png')}/>
   </TouchableOpacity>
